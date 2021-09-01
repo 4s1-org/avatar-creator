022e2e06482aaea05ced966b7ceb5689705a7e67
Steffen <steffen@4s1.de>
2021-09-01 17:44:43 +0200

    stuff

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,43 @@
+import { char } from './chars/char'
+import { Matrix } from './matrix'
+
 export function formatChar(charMatrix: string): string[] {
   return charMatrix.trim().split('\n')
+}
+
+export function upperAlphabet(): Matrix {
+  const matrix = new Matrix()
+  matrix.addChar(char['A'])
+  matrix.addChar(char['B'])
+  matrix.addChar(char['C'])
+  matrix.addChar(char['D'])
+  matrix.addChar(char['E'])
+  matrix.addChar(char['F'])
+  matrix.newLine()
+  matrix.addChar(char['G'])
+  matrix.addChar(char['H'])
+  matrix.addChar(char['I'])
+  matrix.addChar(char['J'])
+  matrix.addChar(char['K'])
+  matrix.addChar(char['L'])
+  matrix.newLine()
+  matrix.addChar(char['M'])
+  matrix.addChar(char['N'])
+  matrix.addChar(char['O'])
+  matrix.addChar(char['P'])
+  matrix.addChar(char['Q'])
+  matrix.addChar(char['R'])
+  matrix.newLine()
+  matrix.addChar(char['S'])
+  matrix.addChar(char['T'])
+  matrix.addChar(char['U'])
+  matrix.addChar(char['V'])
+  matrix.addChar(char['W'])
+  matrix.addChar(char['X'])
+  matrix.newLine()
+  matrix.addChar(char['Y'])
+  matrix.addChar(char['Z'])
+  matrix.makeItPretty()
+  matrix.print()
+  return matrix
 }
